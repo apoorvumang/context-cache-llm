ac3cdbb79913023baf2fb42a53628e897baaf507
Apoorv Saxena <apoorvumang@gmail.com>
2024-08-07 01:53:27 +0530

    hidden state working + fixed token indexing issues for attr. attr implementation still pending tho

--- context_cache_llm.py
+++ context_cache_llm.py
@@ -18,7 +18,10 @@ from mlx_lm.sample_utils import top_p_sampling
 from mlx_lm.tokenizer_utils import TokenizerWrapper, load_tokenizer
 
 class ContextCachingLLM:
-    def __init__(self, model, tokenizer, verbose_time=False, hidden_layer=17):
+    def __init__(self, model, tokenizer, verbose_time=False, 
+                 hidden_layer=20,
+                 save_hidden_states=True,
+                 context_marker="```"):
         self.model = model
         self.tokenizer = tokenizer
         self.cache = None
@@ -26,8 +29,14 @@ class ContextCachingLLM:
         self.messages = []
         self.hidden_layer = hidden_layer
         self.hidden_states = []
+        self.save_hidden_states = save_hidden_states
         self.model.model.output_hidden_states = True
         self.model.model.output_hidden_layer = hidden_layer
+        self.context_text = None
+        self.context_text_parts = None
+        self.context_hidden_states = None
+        self.context_offset_mapping = None
+        self.context_marker = context_marker
 
     def prepare_context(self):
         """
@@ -76,6 +85,10 @@ class ContextCachingLLM:
         self.messages = []
         self.cache = None
         self.hidden_states = []
+        self.context_text = None
+        self.context_hidden_states = None
+        self.context_text_parts = None
+        self.context_offset_mapping = None
         mx.metal.clear_cache()
 
     def _update_cache(self):
@@ -98,17 +111,50 @@ class ContextCachingLLM:
         prefix_tokens = mx.array(self.tokenizer.encode(prefix)).reshape(1, -1)
         logits, hidden_states = self.model(prefix_tokens, cache=self.cache,)
         mx.eval(logits)  # needed since mlx is lazy execution
+        # mx.eval(hidden_states)  # needed since mlx is lazy execution
         end_time = time.time()
         
         time_taken = end_time - start_time
 
         # Store hidden states for the specified layer
-        self.hidden_states = hidden_states
-        print("Hidden states shape: ", hidden_states.shape)
+        # We need to do this only for the 'context' part for now
+        if self.save_hidden_states:
+            self._update_hidden_states(hidden_states)
 
         if self.verbose_time:
             # print time taken to update cache, and token length
             print(f"Time taken to update cache: {time_taken:.3f} seconds for {len(prefix_tokens[0])} tokens.")
+
+    def _update_hidden_states(self, hidden_states):
+        """
+        Store hidden states for the context part.
+        """
+        prefix = self.tokenizer.apply_chat_template(
+            self.messages, tokenize=False, add_generation_prompt=False
+        )
+
+        start_char_index = prefix.find(self.context_marker) + len(self.context_marker)
+        end_char_index = prefix.rfind(self.context_marker)
+        
+        tokenizer_output = self.tokenizer.encode_plus(prefix, return_offsets_mapping=True)
+
+        offset_mapping = fix_offsets(tokenizer_output["offset_mapping"])
+
+        start_token_index = char_to_token_index(start_char_index, offset_mapping) or 0
+        end_token_index = char_to_token_index(end_char_index, offset_mapping) or len(offset_mapping) - 1
+
+        self.context_hidden_states = hidden_states[0][0][start_token_index:end_token_index, :]
+        # get context text parts using offset mapping
+        self.context_text_parts = [prefix[start:end] for start, end in offset_mapping[start_token_index:end_token_index]]
+        self.context_text = "".join(self.context_text_parts)
+        self.context_offset_mapping = [
+            (max(0, start - start_char_index), max(0, end - start_char_index))
+            for start, end in offset_mapping[start_token_index:end_token_index]
+        ]
+        print("Hidden states updated with shape:", self.context_hidden_states.shape)
+        # print context lengths in characters and tokens
+        print(f"Context length: {len(self.context_text)} characters, {len(self.context_text_parts)} tokens.")
+
 
 
     def generate(
@@ -215,8 +261,6 @@ class ContextCachingLLM:
         generated_hidden_states = []
         self.add_message("", role="assistant", update_cache=False) # cache is already updated
 
-        generated_text = ""
-
         for (token, _, token_hidden_state), n in zip(
             self._generate_step(prompt_tokens, **kwargs),
             range(max_tokens),
@@ -224,18 +268,43 @@ class ContextCachingLLM:
             if token == tokenizer.eos_token_id:
                 break
             detokenizer.add_token(token)
-            generated_hidden_states.append(token_hidden_state)
+            generated_hidden_states.append(token_hidden_state[0][-1])
             
             self.update_last_message(detokenizer.text, update_cache=False)
             # Yield the last segment and current hidden states if streaming
-            yield detokenizer.last_segment, mx.stack(generated_hidden_states)
+            yield detokenizer.last_segment, None
 
         detokenizer.finalize()
-        # all_hidden_states = mx.concatenate([self.hidden_states, mx.stack(generated_hidden_states)], axis=0)
-        generated_hidden_states.append(token_hidden_state)
+        # all_hidden_states = mx.concatenate([self.hidden_states, mx.stack(generated_hidden_states)], axis=0) # dunno why this here but not deleting
+        generated_hidden_states.append(token_hidden_state[0][-1])
         self.update_last_message(detokenizer.text, update_cache=False)
         yield detokenizer.last_segment, mx.stack(generated_hidden_states)
 
+    def get_context_attribution(self, hidden_states):
+        """
+        Input: hidden_states of the generated text. Shape: (num_tokens, hidden_dim)
+        Output: attribution of each token in the generated text to the context. Shape: (num_tokens, num_context_tokens)
+        The attribution is calculated using the cosine similarity between the hidden states of the generated text and the context.
+        """
+        # # Ensure hidden_states is a 2D array
+        # if hidden_states.ndim == 1:
+        #     hidden_states = hidden_states.reshape(1, -1)
+        
+        # Normalize the hidden states
+        generated_norm = mx.linalg.norm(hidden_states, axis=1, keepdims=True)
+        context_norm = mx.linalg.norm(self.context_hidden_states, axis=1, keepdims=True)
+
+        # print shapes of norms
+        # print(generated_norm.shape, context_norm.shape)
+        
+        # Compute cosine similarity
+        similarity = (hidden_states @ self.context_hidden_states.T) / (generated_norm * context_norm.T)
+        
+        # Normalize the similarity scores
+        # attribution = mx.softmax(similarity, axis=1)
+        attribution = similarity
+        
+        return attribution
 
     def _generate_step(
         self,
@@ -284,13 +353,14 @@ class ContextCachingLLM:
             if repetition_context_size:
                 if len(repetition_context) > repetition_context_size:
                     repetition_context = repetition_context[-repetition_context_size:]
-            return y, logprobs.squeeze(0), hidden_state[-1]  # Return the last token's hidden state
+            return y, logprobs.squeeze(0), hidden_state[-1]  # Return the last token's hidden state # TODO: this ain't last token thing?
 
-        # Process new tokens (after the prefix)
-        for i in range(start_index, len(y)):
-            _, _, _ = _step(y[i:i+1])
+        # # Process new tokens (after the prefix)
+        # for i in range(start_index, len(y)):
+        #     _, _, _ = _step(y[i:i+1])
+        # y, logprobs, hidden_state = _step(y[-1:])
 
-        y, logprobs, hidden_state = _step(y[-1:])
+        y, logprobs, hidden_state = _step(y[start_index:])
 
         mx.async_eval(y)
         while True:
@@ -298,3 +368,27 @@ class ContextCachingLLM:
             mx.async_eval(next_y)
             yield y.item(), logprobs, hidden_state
             y, logprobs, hidden_state = next_y, next_logprobs, next_hidden_state
+
+
+def fix_offsets(offsets):
+    fixed_offsets = []
+    for i, (start, end) in enumerate(offsets):
+        if i == 0:
+            fixed_offsets.append((start, end))
+        else:
+            prev_start, prev_end = fixed_offsets[-1]
+            
+            # If there's a gap, extend the previous token
+            if prev_end < start:
+                fixed_offsets[-1] = (prev_start, start)
+            
+            # Add the current token as is, preserving zero-width tokens
+            fixed_offsets.append((start, end))
+    
+    return fixed_offsets
+
+def char_to_token_index(char_index, offset_mapping):
+    for token_index, (start, end) in enumerate(offset_mapping):
+        if start <= char_index < end:
+            return token_index
+    return None  # Return None if the character index is out of bounds
--- generate.py
+++ generate.py
@@ -1,7 +1,24 @@
+import numpy as np
+from scipy.signal import find_peaks
 from mlx_lm import load
 from mlx_lm.utils import get_model_path, load_tokenizer
 from context_cache_llm import ContextCachingLLM
 import pdb
+import json
+
+def save_attribution_data(attribution, context_text_parts, generated_text_parts, output_file):
+    # transpose attribution
+    attribution = np.transpose(attribution, (1, 0))
+    data = {
+        "attribution": attribution.tolist(),  # Convert numpy array to list
+        "context_tokens": context_text_parts,
+        "generated_tokens": generated_text_parts
+    }
+    with open(output_file, 'w') as f:
+        json.dump(data, f)
+
+# doc_fname = "large_doc.txt"
+doc_fname = "small_doc.txt"
 
 def main():
     # Load the model and tokenizer
@@ -9,14 +26,17 @@ def main():
     # model, tokenizer = load("mlx-community/Mistral-7B-Instruct-v0.3-4bit")
     # model, tokenizer = load("mlx-community/Qwen2-7B-Instruct-4bit")
     # model, tokenizer = load("mlx-community/Qwen2-1.5B-4bit")
-    model, tokenizer = load("mlx-community/NeuralDaredevil-8B-abliterated-4bit") # this has bad tokenizer
-    tokenizer = load_tokenizer(get_model_path("mlx-community/Meta-Llama-3-8B-Instruct-4bit"))
+    # model, tokenizer = load("mlx-community/NeuralDaredevil-8B-abliterated-4bit") # this has bad tokenizer
+    # tokenizer = load_tokenizer(get_model_path("mlx-community/Meta-Llama-3-8B-Instruct-4bit"))
+
+    # model, tokenizer = load("mlx-community/Meta-Llama-3-8B-Instruct-4bit")
+    model, tokenizer = load("mlx-community/Meta-Llama-3.1-8B-Instruct-4bit")
 
     # Initialize the ContextCachingLLM
     llm_session = ContextCachingLLM(model, tokenizer, verbose_time=True)
 
     # Prepare the context (this can be done when the document is loaded)
-    with open("large_doc.txt", "r") as f:
+    with open(doc_fname, "r") as f:
         doc = f.read()
     
     system_prompt = f"""You are a helpful assistant. The user has selected some content from a website given below as CONTEXT. 
@@ -24,7 +44,9 @@ Please answer the user's questions related to that. Some key points:
 - Always give a direct answer without any prefix or disclaimer.
 - User prefers shorter, to the point answers.
 
-CONTEXT:{doc}"""
+CONTEXT:
+```{doc}```
+"""
     
 #     system_prompt = f"""You are a helpful assistant. I have selected some content from a website given below as CONTEXT. 
 # Please answer the my questions related to that. Some key points:
@@ -40,16 +62,24 @@ CONTEXT:{doc}"""
     
     turn = 0
     while True:
-        # Ask the user for a question
         question = input("Enter your question (or type 'exit' to quit): ")
         if question.lower() == 'exit':
             break
         
-        # Generate the answer
         llm_session.add_message(question, role="user", update_cache=False)
         print("Generated response:")
-        for segment, hidden_states in llm_session.stream_generate(temp=0.7):
+        generated_text_parts = []
+        for segment, hidden_states in llm_session.stream_generate(temp=0.001):
             print(segment, end="", flush=True)
+            generated_text_parts.append(segment)
+            if hidden_states is not None:
+                hidden_states = hidden_states[1:, :]
+                generated_text_parts = generated_text_parts[:-1]
+                attribution = llm_session.get_context_attribution(hidden_states)
+                context_text_parts = llm_session.context_text_parts
+                
+                save_attribution_data(np.array(attribution), context_text_parts, generated_text_parts, 'attribution_data.json')
+                
         print()
         turn += 1
 
